d0f2fda7cc40b8e3be75c2061fec78e51290d698
andres.mera <feli041089@gmail.com>
2024-03-09 10:23:43 -0500

    feature/playlist : Creacion del modulo y el routing.

---
+++ src/app/play-list/play-list.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PlayListRoutingModule } from './play-list-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PlayListRoutingModule
+  ]
+})
+export class PlayListModule { }
