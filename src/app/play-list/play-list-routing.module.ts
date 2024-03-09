9b36a8971d4d288156e68bbe70e2011a98cf1f1f
andres.mera <feli041089@gmail.com>
2024-03-09 10:37:04 -0500

    feature/playlist : Modificacion en los routing para el redireccionamiento del app.

--- src/app/play-list/play-list-routing.module.ts
+++ src/app/play-list/play-list-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'register', component: RegisterPageComponent },
+      { path: 'list', component: ListPageComponent },
+      { path: '**', redirectTo: 'list' },
+    ],
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
