7b12d328af51ac5a5404afa074af4917f25ed749
andres.mera <feli041089@gmail.com>
2024-03-09 08:25:49 -0500

    feature/auth : Configuracion de redireccionamiento de routing.

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'login', component: LoginPageComponent },
+      { path: 'sign-up', component: RegisterPageComponent },
+      { path: '**', redirectTo: 'login' },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
