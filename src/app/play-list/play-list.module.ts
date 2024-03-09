b1dbc4b1478d72eeae4f463f18d1415be638a3fa
andres.mera <feli041089@gmail.com>
2024-03-09 10:27:14 -0500

    feature/playlist : Creacion de los componetes pages de register y list de la playlist.

--- src/app/play-list/play-list.module.ts
+++ src/app/play-list/play-list.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PlayListRoutingModule } from './play-list-routing.module';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegisterPageComponent,
+    ListPageComponent
+  ],
   imports: [
     CommonModule,
     PlayListRoutingModule
